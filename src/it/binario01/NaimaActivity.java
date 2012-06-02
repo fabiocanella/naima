6aae0321d7e1c60e5a3b1592bf290c3b506ca05d
Fabio Canella <info@canella.biz>
2012-06-02 12:05:39 +0200

    progetto eclipse iniziale

---
+++ src/it/binario01/NaimaActivity.java
@@ -0,0 +1,13 @@
+package it.binario01;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class NaimaActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
